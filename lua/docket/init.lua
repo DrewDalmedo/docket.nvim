1298af42a24334a6ab117d0f4e995fe1a53d2294
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-06-06 12:16:00 -0400

    feat(global dockets): create a global floating docket window command

--- lua/docket/init.lua
+++ lua/docket/init.lua
@@ -33,6 +33,56 @@ function M.configure_buffer(bufnr)
 
 	-- don't list in :ls
 	vim.api.nvim_buf_set_option(bufnr, "buflisted", false)
+
+	vim.api.nvim_buf_set_option(bufnr, "modifiable", true)
+end
+
+function M.open_global_floating()
+	ensure_global_dir()
+	local path = get_global_path()
+
+	local bufnr = vim.fn.bufadd(path)
+	vim.fn.bufload(bufnr)
+	M.configure_buffer(bufnr)
+
+	-- compute a reasonable size / position for the floating window:
+	local total_cols = vim.o.columns
+	local total_lines = vim.o.lines
+	local width = math.floor(total_cols * 0.8)
+	local height = math.floor(total_lines * 0.8)
+	local row = math.floor((total_lines - height) / 2 - 1)
+	local col = math.floor((total_cols - width) / 2)
+
+	-- open a floating window for the buffer
+	local win_opts = {
+		relative = "editor",
+		width = width,
+		height = height,
+		row = row,
+		col = col,
+		--style = "minimal",
+		border = "solid",
+	}
+	local win_id = vim.api.nvim_open_win(bufnr, true, win_opts)
+
+	-- make sure edits are written out on BufLeave or VimLeavePre
+	vim.api.nvim_create_autocmd({ "BufLeave", "VimLeavePre" }, {
+		buffer = bufnr,
+		callback = function()
+			if vim.api.nvim_buf_get_option(bufnr, "modified") then
+				vim.cmd("silent write")
+			end
+		end,
+	})
+
+	-- map esc to close the floating window:
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		"n",
+		"<Esc>",
+		string.format("<cmd>silent! bwipeout %d<CR>", bufnr),
+		{ noremap = true, silent = true }
+	)
 end
 
 function M.open_global()
--- plugin/docket.lua
+++ plugin/docket.lua
@@ -11,3 +11,7 @@ end
 vim.api.nvim_create_user_command("DocketGlobal", function()
 	docket.open_global()
 end, { desc = "Open (or switch to) your global docket" })
+
+vim.api.nvim_create_user_command("DocketGlobalFloating", function()
+	docket.open_global_floating()
+end, { desc = "Open global docket in a floating window" })
